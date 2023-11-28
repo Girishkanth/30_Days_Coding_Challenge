0f005a12fc2690de65161afb210840d8dadcccc1
Girishkanth <117515881+Girishkanth@users.noreply.github.com>
2023-11-28 23:17:35 +0530

    Add files via upload

---
+++ Coding Ninjas/Day 11 Sort 0 1 2-CN.cpp
@@ -0,0 +1,52 @@
+/*
+Sort 0 1 2
+
+Problem Statement:
+
+You have been given an integer array/list(ARR) of size 'N'. It only contains Os, 1s and 2s. Write a solution to sort this array/list.
+
+Note:
+Try to solve the problem in 'Single Scan'. 'Single Scan' refers to iterating over the array/list just once or to put it in other words, you will be visiting each element in the array/list just once.
+
+Constraints :
+1 <= T <= 10
+1 <= N <= (5*(10^5))
+0 <= ARR[i] <= 2
+
+Where 'N' is the size of the given array/list.
+And, ARR[I] denotes the i-th element in the array/list.
+
+Sample Input 1 :
+
+2
+6
+0 1 2 2 1 0
+7
+0 1 2 1 2 1 2
+
+Sample Output 1 :
+
+0 0 1 1 2 2
+0 1 1 1 2 2 2
+
+Sample Input 2 :
+
+2
+7
+2 2 2 1 1 1 0
+6
+2 1 2 0 1 0
+
+Sample Output 2 :
+
+0 1 1 1 2 2 2
+0 0 1 1 2 2
+
+*/
+//Solution:
+
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+   sort(arr,arr+n);
+}
